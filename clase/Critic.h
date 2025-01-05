7167b5609379c12e20f6ed2f2034c25e960f61b9
vvladutz <vlad.balanescu@s.unibuc.ro>
2025-01-06 00:30:49 +0200

    creat critic header, cu roluri diferite (clasa derivata din utilizator)

---
+++ clase/Critic.h
@@ -0,0 +1,25 @@
+#ifndef CRITIC_H
+#define CRITIC_H
+#include <iostream>
+#include <string>
+#include <vector>
+#include "Utilizator.h"
+#include "Film.h"
+
+class Critic : public Utilizator {
+public:
+    Critic(const std::string& username, const int varsta, const std::vector<Film>& filme_vazute);
+    virtual ~Critic();
+    Critic(const Critic& other);
+    Critic& operator=(const Critic& other);
+
+    int getRole() const override;
+    void evalueazaFilm(const Film& film, float rating) const override;
+    void scrieRecenzie(const Film& film, const std::string& recenzie) const;
+    void afiseaza(std::ostream &os) const override;
+
+    friend std::ostream& operator<<(std::ostream& os, const Critic& c);
+    friend std::istream& operator>>(std::istream&, Critic& c);
+};
+
+#endif //CRITIC_H
