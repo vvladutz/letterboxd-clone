69d7ac8bbbc1c3d523ef571a75fed8724c89414b
vvladutz <vlad.balanescu@s.unibuc.ro>
2025-01-06 00:36:44 +0200

    creat admin header, clasa derivata din utilizator

---
+++ clase/Admin.h
@@ -0,0 +1,26 @@
+#ifndef ADMIN_H
+#define ADMIN_H
+
+#include <iostream>
+#include <vector>
+#include <string>
+#include "Utilizator.h"
+
+class Admin : public Utilizator {
+public:
+    Admin(const std::string& nume, const int varsta, const std::vector<Film>& filme_vazute);
+    virtual ~Admin();
+    Admin(const Admin& other);
+    Admin& operator=(const Admin& other);
+
+    int getRole() const override;
+    void evalueazaFilm(const Film& film, float rating) const override;
+    void scrieRecenzie(const Film& film, const std::string& recenzie) const;
+    void stergeRecenzie(Film& film, const std::string& recenzie) const;
+    void afiseaza(std::ostream &os) const override;
+
+    friend std::ostream& operator<<(std::ostream& os, const Admin& a);
+    friend std::istream& operator>>(std::istream&, Admin& a);
+};
+
+#endif //ADMIN_H
